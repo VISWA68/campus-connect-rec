c9bf365b8cbfafe0d22caaa82d382021f56c11e4
VISWA68 <viswavikas0607@gmail.com>
2025-08-02 20:25:30 +0530

    changes

--- Frontend/campusconnect/lib/config.dart
+++ Frontend/campusconnect/lib/config.dart
@@ -1,4 +1,3 @@
 class Config {
-  // Server URL configuration
-  static const String serverUrl = 'http://192.168.57.231:5000';  // Update this with your server URL
+  static const String serverUrl = 'http://192.168.57.231:5000'; 
 }
